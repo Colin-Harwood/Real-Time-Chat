3590c8e1bbaa647edcaf0c100a22d4fd9a7146ff
Colin Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-08-14 19:32:26 +0200

    Set up tailwind

--- live-time-chat/src/pages/Home.jsx
+++ live-time-chat/src/pages/Home.jsx
@@ -3,9 +3,11 @@ import './Home.css'
 
 const Home = () => {
   return (
+    <>
     <div>
-      <h1>Test</h1>
+      <h1 class="text-8xl font-bold underline text-blue-800">Test</h1>
     </div>
+    </>
   )
 }
 
