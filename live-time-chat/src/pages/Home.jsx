564ebc335312ac4ed3c1cad2eb461d63d80fa58e
Colin Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-08-14 19:58:05 +0200

    Created base 2 columns for home page

--- live-time-chat/src/pages/Home.jsx
+++ live-time-chat/src/pages/Home.jsx
@@ -4,8 +4,13 @@ import './Home.css'
 const Home = () => {
   return (
     <>
-    <div class="w-100 h-screen flex justify-center items-center">
-      <h1 class="text-8xl font-bold text-purple flex items-center bg-cream">Test</h1>
+    <div class="w-100 h-screen flex justify-center items-center grid grid-cols-2">
+      <div class="flex items-center justify-center h-full">
+        <h1 class="text-8xl font-bold text-purple bg-cream w-1/2 text-center h-1/2">Test</h1>
+      </div>
+      <div class="flex items-center justify-center h-full">
+        <h1 class="text-8xl font-bold text-purple bg-cream w-1/2 text-center h-1/2">Test</h1>
+      </div>
     </div>
     </>
   )
