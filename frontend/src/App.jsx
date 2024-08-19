0ecb2b69567f2637b9cdfddce0cdafe85ab8ae7c
Colin Harwood <138476341+Colin-Harwood@users.noreply.github.com>
2024-08-19 12:46:58 +0200

    Created chat file and updated the routes urls to support seperate random chat urls

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import './App.css'
+import Chat from './pages/Chat'
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/:chat" element={<Chat />} />
     </Routes>
   )
 }
